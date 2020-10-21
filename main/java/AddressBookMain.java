2f9aa76db7ca846964c98e68af46a6d044d0ee91
Sanjana Jain <sanjana.jain04@gmail.com>
2020-10-21 06:29:59 +0530

    Removing clashes

--- main/java/AddressBookMain.java
+++ main/java/AddressBookMain.java
@@ -42,6 +42,27 @@ public class AddressBookMain {
             System.out.print("Enter zip: ");
             contact.zip = in.nextLong();
         }
+        in.nextLine();
+        System.out.print("Enter the contact first name to edit: ");
+        if(searchName.equals(contact.firstName))
+        {
+            System.out.print("Enter first name: ");
+            contact.firstName = in.nextLine();
+            System.out.print("Enter last name: ");
+            contact.lastName = in.nextLine();
+            System.out.print("Enter address: ");
+            contact.address = in.nextLine();
+            System.out.print("Enter city: ");
+            contact.city = in.nextLine();
+            System.out.print("Enter state: ");
+            contact.state = in.nextLine();
+            System.out.print("Enter phoneNumber: ");
+            contact.phoneNumber = in.nextLine();
+            System.out.print("Enter email: ");
+            contact.email = in.nextLine();
+            System.out.print("Enter zip: ");
+            contact.zip = in.nextLong();
+        }
         in.close();
     }
 }
