80e201aed81bcf6b94a121674c9b7ff770ac4ec3
Sanjana Jain <sanjana.jain04@gmail.com>
2020-11-05 12:40:32 +0530

    Refactored-Hashmap

--- main/java/AddressBookMain.java
+++ main/java/AddressBookMain.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class AddressBookMain {
-    public static ArrayList<AddressBook> addressBookList = new ArrayList<AddressBook>();
+    public static HashMap<String,AddressBook> addressBookMap = new HashMap<String,AddressBook>();
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int choice = 0;
@@ -35,51 +35,52 @@ public class AddressBookMain {
                     System.out.print("Enter zip: ");
                     contact.zip = in.nextLong();
                     in.nextLine();
-                    addressBookList.add(contact);
+                    addressBookMap.put(contact.firstName,contact);
                     break;
                 }
                 case 2:
                 {
                     System.out.print("Enter the contact first name to edit: ");
                     String searchName = in.nextLine();
-                    for(int i=0;i<addressBookList.size();i++)
+                    if(addressBookMap.containsKey(searchName))
                     {
-                        if(searchName.equals(addressBookList.get(i).firstName))
-                        {
-                            System.out.print("Enter first name: ");
-                            addressBookList.get(i).firstName = in.nextLine();
-                            System.out.print("Enter last name: ");
-                            addressBookList.get(i).lastName = in.nextLine();
-                            System.out.print("Enter address: ");
-                            addressBookList.get(i).address = in.nextLine();
-                            System.out.print("Enter city: ");
-                            addressBookList.get(i).city = in.nextLine();
-                            System.out.print("Enter state: ");
-                            addressBookList.get(i).state = in.nextLine();
-                            System.out.print("Enter phoneNumber: ");
-                            addressBookList.get(i).phoneNumber = in.nextLine();
-                            System.out.print("Enter email: ");
-                            addressBookList.get(i).email = in.nextLine();
-                            System.out.print("Enter zip: ");
-                            addressBookList.get(i).zip = in.nextLong();
-                            in.nextLine();
-                            break;
+                        AddressBook contact = new AddressBook();
+                        System.out.print("Enter first name: ");
+                        contact.firstName = in.nextLine();
+                        System.out.print("Enter last name: ");
+                        contact.lastName = in.nextLine();
+                        System.out.print("Enter address: ");
+                        contact.address = in.nextLine();
+                        System.out.print("Enter city: ");
+                        contact.city = in.nextLine();
+                        System.out.print("Enter state: ");
+                        contact.state = in.nextLine();
+                        System.out.print("Enter phoneNumber: ");
+                        contact.phoneNumber = in.nextLine();
+                        System.out.print("Enter email: ");
+                        contact.email = in.nextLine();
+                        System.out.print("Enter zip: ");
+                        contact.zip = in.nextLong();
+                        in.nextLine();
+                        addressBookMap.remove(searchName);
+                        addressBookMap.put(contact.firstName,contact);
+                        System.out.println("Contact modified");
                         }
-                    }
+                    else
+                        System.out.println("Contact not found");
                     break;
                 }
                 case 3:
                 {
                     System.out.print("Enter the contact first name to delete: ");
                     String searchName = in.nextLine();
-                    for(int i=0;i<addressBookList.size();i++)
+                    if(addressBookMap.containsKey(searchName))
                     {
-                        if(searchName.equals(addressBookList.get(i).firstName))
-                        {
-                            addressBookList.remove(i);
-                            System.out.println("Contact deleted successfully");
-                        }
+                        addressBookMap.remove(searchName);
+                        System.out.println("Contact deleted successfully");
                     }
+                    else
+                        System.out.println("Contact not found");
                 }
             }
         }while(choice!=4);
