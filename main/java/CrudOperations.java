7e993c5e8f199578e9c04175cc15c9aae859ac87
Sanjana Jain <sanjana.jain04@gmail.com>
2020-11-11 01:25:45 +0530

    Added-UC17 Updating Contacts in the database

--- main/java/CrudOperations.java
+++ main/java/CrudOperations.java
@@ -1,8 +1,8 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -50,10 +50,67 @@ public class CrudOperations {
                     addressBook.contactList.add(contact);
                 }
             }
-            displayAddressBooks();
         } catch(SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public Contact readByName(String firstName) {
+        try {
+            Connection con = JDBCConnection.getInstance().getConnection();
+            String query = "select ab.ab_name, c.contact_id, c.first_name, c.last_name, c.phone_no, c.email, a.address, a.city, a.state, a.zip, abt.abt_name " +
+                    "from address_book ab " +
+                    "right join contact c on ab.ab_id = c.ab_id " +
+                    "inner join address a on c.contact_id = a.contact_id " +
+                    "left join (select contact_id, abt_name from contact_type, address_book_type " +
+                    "where contact_type.abt_id = address_book_type.abt_id) abt " +
+                    "on c.contact_id = abt.contact_id " +
+                    "where c.first_name = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setString(1, firstName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Contact contact = null;
+            while(resultSet.next()) {
+                if(contact == null) {
+                    contact = new Contact();
+                    contact.setContact_id(resultSet.getInt("contact_id"));
+                    contact.setFirstName(resultSet.getString("first_name"));
+                    contact.setLastName(resultSet.getString("last_name"));
+                    contact.setPhoneNumber(resultSet.getString("phone_no"));
+                    contact.setEmail(resultSet.getString("email"));
+                    contact.setAddress(resultSet.getString("address"));
+                    contact.setCity(resultSet.getString("city"));
+                    contact.setState(resultSet.getString("state"));
+                    contact.setZip(resultSet.getInt("zip"));
+                    contact.type.add(resultSet.getString("abt_name"));
+                }
+                else
+                    contact.type.add(resultSet.getString("abt_name"));
+            }
+            return contact;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void updateContact(String firstName, String phone) {
+        try {
+            Connection con = JDBCConnection.getInstance().getConnection();
+            Statement stmt = con.createStatement();
+            String query = "update contact set phone_no = '" + phone + "' where first_name = '" + firstName + "'";
+            System.out.println("Updating phone number for " + firstName +"... ");
+            stmt.executeUpdate(query);
+            System.out.println("Phone number updated");
+            sync();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sync() {
+        AddressBookMain.addressBookMap.clear();
+        readAll();
     }
 
     private Contact addressBookHasContactId(AddressBook addressBook, int contact_id) {
@@ -67,7 +124,7 @@ public class CrudOperations {
         return null;
     }
 
-    private void displayAddressBooks() {
+    public void displayAddressBooks() {
         for(Map.Entry<String, AddressBook> entry : AddressBookMain.addressBookMap.entrySet()) {
             System.out.println("AddressBook Name: " + entry.getKey());
             AddressBook addressBook = entry.getValue();
--- test/java/AddressBookServiceTest.java
+++ test/java/AddressBookServiceTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class AddressBookServiceTest {
 
     @Test
-    public void test() {
+    public void givenDatabase_WhenRetrieved_ShouldreturnCorrectCount() {
         try {
             Connection con = JDBCConnection.getInstance().getConnection();
             Statement stmt = con.createStatement();
@@ -19,6 +19,7 @@ public class AddressBookServiceTest {
             int expectedOutput = resultSet.getInt("count(contact_id)");
             CrudOperations crudOperations = new CrudOperations();
             crudOperations.readAll();
+            crudOperations.displayAddressBooks();
             int output = AddressBookMain.addressBookMap.get("AB1").contactList.size();
             Assert.assertEquals(expectedOutput, output);
         } catch (SQLException e) {
@@ -26,4 +27,24 @@ public class AddressBookServiceTest {
         }
     }
 
+    @Test
+    public void givenContactName_WhenPhoneUpdated_ShouldSync() {
+        CrudOperations crudOperations = new CrudOperations();
+        crudOperations.updateContact("Kumar", "1111111111");
+        Contact dbContact = crudOperations.readByName("Kumar");
+        Contact mapContact = null;
+        for(Contact contact : AddressBookMain.addressBookMap.get("AB1").contactList) {
+            if(contact.getFirstName().equals("Kumar"))
+                mapContact = contact;
+        }
+        Assert.assertTrue(dbContact.equalsObject(mapContact));
+        crudOperations.updateContact("Kumar", "2222222222");
+        dbContact = crudOperations.readByName("Kumar");
+        mapContact = null;
+        for(Contact contact : AddressBookMain.addressBookMap.get("AB1").contactList) {
+            if(contact.getFirstName().equals("Kumar"))
+                mapContact = contact;
+        }
+        Assert.assertTrue(dbContact.equalsObject(mapContact));
+    }
 }
